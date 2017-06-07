6157da4660b50dae36dedbe2d692d2cb0d6d480a
gdmec07150938 <WXJ907468952@outlook.com>
2017-06-07 10:52:31 +0800

    添加缓存

--- src/common/js/sw.js
+++ src/common/js/sw.js
@@ -13,6 +13,17 @@ this.addEventListener('install', function (event) {
 
 this.addEventListener('fetch', function (event) {
   console.log(event.request)
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (resp) {
+        return resp || fetch(event.request).then(function (response) {
+            return caches.open('v1').then(function (cache) {
+              cache.put(event.request, response.clone())
+              return response
+            })
+          })
+      })
+  )
 })
 
 this.addEventListener('activate', function (event) {
